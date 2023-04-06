e14aa69d7322b6c68d7fdd1421ccf554e762e904
IvanF <60158053+Bot-Ivan@users.noreply.github.com>
2023-04-06 13:29:23 -0400

    Importing libraries

---
+++ EmotionalSpeechClassifier.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+import librosa
+import librosa.display
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
+
+import keras
+from keras.callbacks import ReduceLROnPlateau
+from keras.models import Sequential
+from keras.layers import Dense, Conv1D, MaxPooling1D, Flatten, Dropout, BatchNormalization
+from keras.utils import np_utils, to_categorical
+from keras.callbacks import ModelCheckpoint
